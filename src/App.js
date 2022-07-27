7c5b83023d72c8e8ea99e3f31282c7a949bc03c8
Andriy <klym.andriy14@gmail.com>
2022-07-27 18:14:32 +0300

    changed to hooks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import {useRef, useState, useEffect} from 'react';
+import {Container} from 'react-bootstrap';
 import './App.css';
 
+const Form = () => {
+
+    const [text, setText] = useState('');
+
+    const myRef = useRef(1);
+
+    // count how many times the element has been rerendered
+    useEffect(() => {
+        myRef.current++
+        console.log(myRef.current);
+    })
+
+    return (
+        <Container>
+            <form className="w-50 border mt-5 p-3 m-auto">
+                <div className="mb-3">
+                    <label htmlFor="exampleFormControlInput1" className="form-label">Email address</label>
+                    <input onChange={(e) => setText(e.target.value)} type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
+                    </div>
+                    <div className="mb-3">
+                    <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
+                    <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                </div>
+            </form>
+        </Container>
+    )
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Form/>
+    );
 }
 
 export default App;
+
